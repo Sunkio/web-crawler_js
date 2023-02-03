e04dd190cc03abbd0da6c6876d68eb2a1cb36b2d
Tanja Schmidt <taschmi@gmx.net>
2023-02-03 17:42:16 +0100

    add normalizeURL + tests

--- crawl.js
+++ crawl.js
@@ -1,5 +1,14 @@
-const normalizeURL = () => {
+const normalizeURL = (urlStr) => {
+    const urlObj = new URL(urlStr);
+    console.log(urlObj);
+    const { hostname, pathname } = urlObj;
+    const pathNameLower = pathname.toLowerCase();
+    const hostPath = `${hostname}${pathNameLower}`;
 
+    if (hostPath.length > 0 && hostPath.endsWith('/')) {
+        return hostPath.slice(0, -1);
+    }
+    return hostPath;
 };
 
 module.exports = {
--- crawl.test.js
+++ crawl.test.js
@@ -1,11 +1,30 @@
-// use regex to match the url - one test for each possible version(?) of the url
-// take lane's personal blog as a test instance (what you send into the test function)
-// and test for everything else you can think of
-
-
 const { test, expect } = require('@jest/globals');
 const { normalizeURL } = require('./crawl.js');
 
-test('normalizeURL should return a string', () => {
-    expect(typeof normalizeURL('https://www.google.com/')).toBe('string');
+test('normalizeURL should only return a hostname/pathname string', () => {
+    const input = "https://blog.boot.dev/path";
+    const actual = normalizeURL(input);
+    const expected = "blog.boot.dev/path";
+    expect(actual).toEqual(expected);
+});
+
+test('normalizeURL should trim a slash at the end', () => {
+    const input = "https://blog.boot.dev/path/";
+    const actual = normalizeURL(input);
+    const expected = "blog.boot.dev/path";
+    expect(actual).toEqual(expected);
+});
+
+test('normalizeURL should turn uppercase letters to lowercase', () => {
+    const input = "https://blog.BOOT.dev/PATH";
+    const actual = normalizeURL(input);
+    const expected = "blog.boot.dev/path";
+    expect(actual).toEqual(expected);
+});
+
+test('normalizeURL should work with a regular HTTP URL too', () => {
+    const input = "http://blog.boot.dev/path";
+    const actual = normalizeURL(input);
+    const expected = "blog.boot.dev/path";
+    expect(actual).toEqual(expected);
 });
