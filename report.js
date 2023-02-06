c3e30adf958c70dbd188604a46beba6adc128251
Tanja Schmidt <taschmi@gmx.net>
2023-02-06 13:01:17 +0100

    add export report to csv

--- report.js
+++ report.js
@@ -1,3 +1,32 @@
+'use strict'
+const fs = require('fs');
+const { Parser } = require('json2csv');
+
+
+const createCsv = (sortedPages) => {
+    const fields = ['url', 'count'];
+    const pagesObj = sortedPages.map((page) => {
+        return {
+            url: page[0],
+            count: page[1]
+        }
+    });
+    const csv = new Parser({fields});
+    fs.writeFile('./reports/crawl-report.csv', csv.parse(pagesObj) , function (err) {
+        if (err) throw err;
+        console.log('CSV file created and saved in reports directory!');
+    });
+}
+
+const sortPages = (pages) => {
+    const pagesArr = Object.entries(pages);
+    pagesArr.sort((a, b) => {
+        return b[1] - a[1];
+    })
+
+    return pagesArr;
+}
+
 function printReport(pages) {
     const sortedPages = sortPages(pages);
     console.log(`
@@ -12,19 +41,10 @@ function printReport(pages) {
         END OF REPORT
         ============
         `);
-}
-
-
-const sortPages = (pages) => {
-    const pagesArr = Object.entries(pages);
-    pagesArr.sort((a, b) => {
-        return b[1] - a[1];
-    })
-
-    return pagesArr;
+    createCsv(sortedPages);
 }
 
 module.exports = {
     sortPages,
-    printReport,
+    printReport
 }
